8632d54535328557b2180f0d2dcfa67b2362706b
Elizabeth <zeereigns@hotmail.com>
2025-08-04 06:56:46 +0100

    Add and style footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import './App.css';
 import PostsPage from './features/posts/PostPage';
 import PostDetailPage from './features/posts/PostDetailPage';
+import Footer from './features/footer/Footer';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
      <Route path='/'element={ <PostsPage /> } />
      <Route path = '/post/:postId' element={<PostDetailPage />} />
           </Routes>
+          <Footer />
          </div>
            
   );
---
+++ src/features/footer/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from './footer.module.css'
+
+function Footer() {
+    return(
+        <h4 className={styles.footer}>Reddit Client v1.0 • Built with ❤️ by leezab3th</h4>
+    )
+}
+
+export default Footer;
