bf91a56031fce1e96523f8a684844619cd334b07
Elizabeth <zeereigns@hotmail.com>
2025-12-30 05:52:58 +0000

    fix: install backend deps and route /api to lambda

--- backend/lambda.js
+++ backend/lambda.js
@@ -1,8 +1,4 @@
 const serverless = require('serverless-http');
 const app = require('./app');
 
-const handler = process.env.NETLIFY_DEV 
-  ? serverless(app, { basePath: '/api' })
-  : serverless(app);
-
-module.exports.handler = handler;
+module.exports.handler = serverless(app);
