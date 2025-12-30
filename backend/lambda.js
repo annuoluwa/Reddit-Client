c0a14a5eeaacb3af52808c578906ae9e48937b55
Elizabeth <zeereigns@hotmail.com>
2025-12-30 06:47:40 +0000

    Fix lambda filename for Netlify functions

--- backend/lambda.js
+++ backend/lambda.js
@@ -1,6 +1,4 @@
 const serverless = require('serverless-http');
 const app = require('./app');
 
-module.exports.handler = serverless(app, {
-  basePath: '/.netlify/functions/lambda'
-});
+module.exports.handler = serverless(app);
